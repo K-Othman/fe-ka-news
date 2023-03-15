694cf3329e4b26d4d103879dbf6f00dbe3ed0bdb
K-Othman <karim_othman88@hotmail.com>
2023-03-15 16:36:52 +0000

    working on styling the article page and the comment section

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -5,6 +5,7 @@ import "./Article.scss";
 import Comments from "../Comments/Comments";
 import Vote from "./Vote";
 import { Error404 } from "../Error404";
+import * as React from "react";
 
 function Article({ setErr, err }) {
   const [article, setArticle] = useState({});
@@ -29,21 +30,44 @@ function Article({ setErr, err }) {
   const date = new Date(article.created_at);
 
   return (
-    <section className="article container ">
-      <p className="date"> {date.toLocaleDateString()} </p>
-      <h4> {article.title} </h4>
-      <p className="body">{article.body}</p>
-      <p className="author"> {article.author} </p>
-      <p className="topic"> {article.topic} </p>
+    <section>
+      <div className="article container ">
+        <div className="article_header">
+          <p className="date"> Published at {date.toLocaleDateString()} </p>
+          <p className="topic"> {article.topic} </p>
+        </div>
+        <h4> {article.title} </h4>
+        <div className="author_like">
+          <p className="author">By {article.author} </p>
+          <div className="like">
+            <p>
+              {article.votes}{" "}
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  className="likes_icon"
+                  d="M885.9 533.7c16.8-22.2 26.1-49.4 26.1-77.7 0-44.9-25.1-87.4-65.5-111.1a67.67 67.67 0 00-34.3-9.3H572.4l6-122.9c1.4-29.7-9.1-57.9-29.5-79.4A106.62 106.62 0 00471 99.9c-52 0-98 35-111.8 85.1l-85.9 311h-.3v428h472.3c9.2 0 18.2-1.8 26.5-5.4 47.6-20.3 78.3-66.8 78.3-118.4 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7-.2-12.6-2-25.1-5.6-37.1zM112 528v364c0 17.7 14.3 32 32 32h65V496h-65c-17.7 0-32 14.3-32 32z"
+                />
+              </svg>
+            </p>
+            <p className="recom">Recommencations</p>
+          </div>
+        </div>
+        <p className="body">{article.body}</p>
 
-      <Vote
-        article_id={article.article_id}
-        votes={article.votes}
-        setErr={setErr}
-        err={err}
-      />
+        <Vote
+          article_id={article.article_id}
+          votes={article.votes}
+          setErr={setErr}
+          err={err}
+        />
+      </div>
 
-      <Comments className="comments" article_id={article_id} />
+      <Comments className="article " article_id={article_id} />
     </section>
   );
 }
--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -23,18 +23,31 @@ function Comments({ article_id }) {
   };
 
   return (
-    <>
-      <h3>Comments</h3>
-      <div className="comments">
+    <section className="comments container">
+      <div className="comment_header">
+        <svg
+          className="comment_icon"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          height="1em"
+          width="1em"
+        >
+          <path d="M19 8h-1V5a3 3 0 00-3-3H5a3 3 0 00-3 3v12a1 1 0 00.62.92A.84.84 0 003 18a1 1 0 00.71-.29l2.81-2.82H8v1.44a3 3 0 003 3h6.92l2.37 2.38A1 1 0 0021 22a.84.84 0 00.38-.08A1 1 0 0022 21V11a3 3 0 00-3-3zM8 11v1.89H6.11a1 1 0 00-.71.29L4 14.59V5a1 1 0 011-1h10a1 1 0 011 1v3h-5a3 3 0 00-3 3zm12 7.59l-1-1a1 1 0 00-.71-.3H11a1 1 0 01-1-1V11a1 1 0 011-1h8a1 1 0 011 1z" />
+        </svg>
+        <h3 className="comment_title">Comments</h3>
+      </div>
+      <div className="">
         <AddComment article_id={article_id} setComments={setComments} />
         {comments.map((comment) => {
           const date = new Date(comment.created_at);
           return (
             <div key={comment.created_at} className={"commentHolder"}>
+              {/* <div> */}
               <h5> {comment.author} </h5>
               <p className="body"> {comment.body} </p>
               <p className="date">Comment date: {date.toLocaleDateString()}</p>
               <p className="votes"> Votes: {comment.votes} </p>
+              {/* </div> */}
               <button onClick={() => deleteCommentHandle(comment.comment_id)}>
                 Delete
               </button>
@@ -42,7 +55,7 @@ function Comments({ article_id }) {
           );
         })}
       </div>
-    </>
+    </section>
   );
 }
 
